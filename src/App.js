286345925eadbd6228de776ede0d30a867dc28e7
arphad <arphad@naver.com>
2025-09-15 23:44:12 +0900

    다시3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. 123 456 789
-          123 456
+          123 456 7
         </p>
         <a
           className="App-link"
